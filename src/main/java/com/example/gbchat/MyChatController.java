f14429a2a4f583bbdb9999076eaf82ba88d91114
m4sbobkov <alexbob14@mail.ru>
2021-11-22 12:59:36 +0400

    Initial commit

---
+++ src/main/java/com/example/gbchat/MyChatController.java
@@ -0,0 +1,25 @@
+package com.example.gbchat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MyChatController {
+
+    @FXML
+    TextArea mainTextArea;
+
+    @FXML
+    TextField message;
+
+
+    @FXML
+    protected void onSendButtonClick(ActionEvent actionEvent) {
+        if (!message.getText().isEmpty()) {
+            mainTextArea.appendText(message.getText() + "\n");
+            message.clear();
+        }
+    }
+}
